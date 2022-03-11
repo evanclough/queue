32434593cda7f65c74270f4ba306b3b1f1c28c29
Evan Clough <evanclough99@gmail.com>
2022-03-10 23:51:14 -0500

    integrated db to server again

--- server/server.py
+++ server/server.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'queue'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+#query db with:
+#with mysql.connect() as connection
+#if insert, connection.commit
+#if pulling, cursor.fetchone or cursor.fetchall
 
 @socketio.on('connect')
 def connect(auth):
