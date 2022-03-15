464bcb65d998344afe2e40d5d085f6af2d2dd6b1
evanclough <evanclough99@gmail.com>
2022-03-15 01:05:37 -0400

    made namespace class for different rooms

--- server/server.py
+++ server/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, Namespace
 from flaskext.mysql import MySQL
 import time
 import html
 import requests
+from queue import Queue
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -22,52 +23,38 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 #if insert, connection.commit
 #if pulling, cursor.fetchone or cursor.fetchall
 
-@socketio.on('connect')
-def connect(auth):
-    print('connected')
-    #emit queue
-    emit('my response', {'data': 'Connected'})
+class Room(Namespace):
+    def __init__(self, route):
+        super().__init__(route)
+        self.queue = Queue()
+        self.connected = 0
+    def on_connect(self):
+        print('connected')
+        emit("current_videos", list(self.queue.queue))
+        emit("connected_users", {"connectedUsers": self.connected})
+        self.connected+=1
+    def on_disconnect(self):
+        print('client_disconnected')
+        self.connected-=1
+    def on_link_input(self, data):
+        link = html.escape(data["link"])
+        #check if link is valid youtube video
+        VIDEO_ID = link[len(link) - 11:]
+        r = requests.get(f"https://www.youtube.com/watch?v={VIDEO_ID}")
+        if "Video unavailable" in r.text:
+            print(123)
+            emit("input_status", {"success": False})
+            return
+        self.queue.put(VIDEO_ID)
+        emit("add_video", link, broadcast=True)
+        emit("input_status", {"success": True})
+    def on_get_current_video(self):
+        emit('get_current_video', {"ID": "-1"} if self.queue.empty() else self.queue.get())
 
-@socketio.on('disconnect')
-def disconnect():
-    print('Client disconnected')
-    
-@socketio.on('link_input')
-def link_input(link):
-    link = html.escape(link)
-    #check if link is valid youtube video
-    VIDEO_ID = link[len(link) - 11:]
-    r = requests.get(f"https://www.youtube.com/watch?v={VIDEO_ID}")
-    if "Video unavailable" in r.text:
-        print(123)
-        emit("input_status", {"success": False})
-        return
-    #update database
-
-    #send out video to everyone in room
-    emit("add_video", link, broadcast=True)
-    emit("input_status", {"success": True})
-
-@socketio.on('get_current_video')
-def get_current_video():
-    #grab current video from DB
-    socketio.emit("switch_video", {"videoID": "1nzuUprovC4"})
-
-def main_loop():
-        pass
-        #if current video is over, 
-        #then emit switch_video with next video in db
-        #set time started to now, 
-        #and edit DB accordingly.
-        #if there isn't a next video, 
-        #then emit switch_video with null or
-        #whatever flag I choose.
-
-def main_loop(socketio):
-    while True:
-        socketio.emit("abc", "123")
-        time.sleep(1)
+#query db for rooms
+rooms = ["a", "b", "c"]
+for room in rooms:
+    socketio.on_namespace(Room(f'/{room}'))
 
 if __name__ == '__main__':
-    SocketIO.start_background_task(main_loop(socketio))
     socketio.run(app)
